e6081b6a81c9e338f095fd2faf2471ee4a38b3de
rizwan <rizwanahamadcodes@gmail.com>
2024-05-07 09:38:27 +0545

    added a drawer for the favorites

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,43 @@
 import clsx from "clsx";
 import Container from "../Container/Container";
 import NavMenuWithTabIndicator from "./NavMenuWithTabIndicator";
-import NavDrawer from "../NavDrawer/NavDrawer";
+import NavDrawer from "../Drawers/NavDrawer";
 import BrandLogo from "../BrandLogo";
 import CartIcon from "../CartIcon";
+import { useToggle } from "../../hooks/useToggle";
+import Drawer, { DrawerBody, DrawerDefaultHead } from "../Drawer/Drawer";
 
-type NavbarProps = {
-    viewportTouchingStatus?: {
-        topTouchedTop: boolean;
-        bottomTouchedTop: boolean;
-    };
-};
-
-const Navbar = (props: NavbarProps) => {
-    const { viewportTouchingStatus } = props;
-
+const Navbar = () => {
+    const {
+        isOpen: isFavoritesOpen,
+        close: closeFavorites,
+        open: openFavorites,
+    } = useToggle(false);
     return (
         <div>
             <nav
                 className={clsx(
-                    "fixed top-0 z-10 w-full bg-white/50 backdrop-blur-sm transition-all dark:bg-gray-900/50",
-                    viewportTouchingStatus?.topTouchedTop
-                        ? "h-navHeight-large"
-                        : "h-navHeight-small shadow"
+                    "fixed top-0 z-10 w-full bg-white/50 backdrop-blur-sm transition-all dark:bg-gray-900/50 h-navHeight-large shadow"
                 )}>
                 <Container className="h-full flex items-center justify-between gap-2">
                     <BrandLogo />
                     <NavMenuWithTabIndicator className="ml-auto hidden lg:block" />
-                    <CartIcon />
+                    <CartIcon open={openFavorites} />
                     <NavDrawer />
+
+                    <Drawer
+                        isOpen={isFavoritesOpen}
+                        close={closeFavorites}
+                        open={openFavorites}>
+                        <DrawerDefaultHead height={"h-navHeight"} />
+                        <DrawerBody>
+                            hello
+                            {/* <NavMenuWithTabIndicator
+                                onNavItemClick={close}
+                                direction="vertical"
+                            /> */}
+                        </DrawerBody>
+                    </Drawer>
                 </Container>
             </nav>
         </div>
